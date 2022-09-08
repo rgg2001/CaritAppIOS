ae34470c832eb6d623995c48fb57c8aa69b6fcf9
Alberto Estrada <a01276671@itesm.mx>
2022-09-07 23:25:18 -0500

    nav bar

---
+++ CaritAppLogin/CaritAppLogin/ProyectsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProyectsViewController.swift
+//  CaritAppLogin
+//
+//  Created by Alberto Estrada on 07/09/22.
+//
+
+import UIKit
+
+class ProyectsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
