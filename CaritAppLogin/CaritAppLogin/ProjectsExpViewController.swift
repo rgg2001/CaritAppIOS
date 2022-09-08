0f963002e2bfb7592f95db83ff1b6235b1e23354
Alberto Estrada <a01276671@itesm.mx>
2022-09-08 01:12:31 -0500

    Proyectos

--- CaritAppLogin/CaritAppLogin/ProjectsExpViewController.swift
+++ CaritAppLogin/CaritAppLogin/ProjectsExpViewController.swift
@@ -1,24 +1,23 @@
 //
-//  ProjectsViewController.swift
+//  ProjectsExpViewController.swift
 //  CaritAppLogin
 //
-//  Created by Alberto Estrada on 07/09/22.
+//  Created by Alberto Estrada on 08/09/22.
 //
 
 import UIKit
 
-class ProjectsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ProjectsExpViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
     
     let images = ["alimentos", "ropa", "medicamentos", "emergencia"]
-    let titles = ["Banco de alimentos", "Banco de Ropa", "Banco de Medicamentos", "Campañas de Emergencia"]
+    let titles = ["Banco de alimentos", "Banco de Ropa y Artículos Varios", "Banco de Medicamentos", "Campañas de Emergencia"]
     let location = ["📍San Pedro Garza Garcia, N.L.","📍San Pedro Garza Garcia, N.L.", "📍San Pedro Garza Garcia, N.L.", "📍San Pedro Garza Garcia, N.L."]
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "post", for: indexPath) as! Post
         cell.image.image = UIImage(named: images[indexPath.row])
         cell.title.text = titles[indexPath.row]
@@ -27,24 +26,29 @@ class ProjectsViewController: UIViewController, UICollectionViewDelegate, UIColl
         return cell
     }
     
+    
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
     
-}
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
 
 class Post: UICollectionViewCell {
     
-    @IBOutlet weak var background: UIView!
-    @IBOutlet weak var image: UIImageView!
-    @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var location: UILabel!
     
-    override func awakeFromNib() {
-        background.layer.cornerRadius = 12
-        image.layer.cornerRadius = 12
-    }
 }
