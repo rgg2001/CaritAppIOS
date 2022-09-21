99e99b8fa8ca8896ca50652462008352f47d14c7
LauHdzV <45604872+LauHdzV@users.noreply.github.com>
2022-09-20 20:25:29 -0500

    codigosQR

--- CaritAppLogin/CaritAppLogin/MyProjectsViewController.swift
+++ CaritAppLogin/CaritAppLogin/MyProjectsViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class MyProjectsViewController: UIViewController {
 
+    
+    @IBOutlet weak var displayCodeView: UIImageView!
+    
+    var filter:CIFilter!
+    
+    let texto = "user123@gmail.com"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let data = texto.data(using: .ascii, allowLossyConversion: false)
+                    
+        filter = CIFilter(name: "CIQRCodeGenerator")
+                    
+        filter.setValue(data, forKey: "inputMessage")
+        let transform = CGAffineTransform(scaleX: 10, y: 10)
+        let imagen = UIImage(ciImage: filter.outputImage!.transformed(by: transform))
+                    
+        displayCodeView.image = imagen
+        
         // Do any additional setup after loading the view.
     }
     
